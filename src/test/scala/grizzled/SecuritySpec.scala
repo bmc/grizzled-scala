44af63a65174c574d2ecaa186c201b61ec0b5592
Adam Lesperance <adam.lesperance@target.com>
2016-11-14 17:44:59 -0600

    Add failing test

--- src/test/scala/grizzled/SecuritySpec.scala
+++ src/test/scala/grizzled/SecuritySpec.scala
@@ -9,8 +9,10 @@ import java.io.ByteArrayInputStream
  */
 class SecuritySpec extends BaseSpec {
   val Data = Array(
-    ("sha-256", "foo") -> "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae",
-    ("md5", "foo")     -> "acbd18db4cc2f85cedef654fccc4a4d8"
+    ("sha-256", "foo")  -> "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae",
+    ("md5", "foo")      -> "acbd18db4cc2f85cedef654fccc4a4d8",
+    ("md5", "ab")       -> "187ef4436122d1cc2f40dc2b92f0eba0",
+    ("md5", "a\u0000b") -> "70350f6027bce3713f6b76473084309b"
   )
 
   "MessageDigest" should "work with string inputs" in {
